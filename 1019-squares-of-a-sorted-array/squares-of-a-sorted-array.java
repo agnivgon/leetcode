6c36f24e33821244a4e772ac7132c1a290b0994e
Agniv Gon <agniv.gon@gmail.com>
2024-08-03 20:41:47 -0700

    Time: 1 ms (100.00%) | Memory: 47.4 MB (14.00%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -2,24 +2,19 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         // https://www.youtube.com/watch?v=fikdT0AI5z4
         // TC: O(N) and SC: O(N)
-        // First loop thorugh the array and square the result
-        // Then use two pointers approach, use left and right pointer
-        // Check if the left pointer value is greater than the right pointer, 
-        // if yes then add left value in the position and increase the left pointer
-        // if not add right value in the position and decrease the right pointer
-        int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
-        }
-        int left = 0;
-        int right = nums.length - 1;
-
-        for (int pos = nums.length - 1; pos >= 0; pos--) {
-            if (nums[left] > nums[right]) {
-                result[pos] = nums[left];
+        // Use two pointers approach, use left and right pointer
+        // Check if the Absolute value of left pointer number is greater than the right pointer, 
+        // if yes then add squares of left value in the position and increase the left pointer
+        // if not add squares of right value in the position and decrease the right pointer
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        for (int pos = n - 1; pos >= 0; pos--) {
+            if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+                result[pos] = nums[left] * nums[left];
                 left++;
             } else {
-                result[pos] = nums[right];
+                result[pos] = nums[right] * nums[right];
                 right--;
             }
         }
